b0555a6d3cb4b781a4e7a9ae1301d983a96f250d
debopamghosh12 <debopamghosh2004@gmail.com>
2025-12-09 21:46:10 +0530

    Fix Vercel Cannot GET error

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-require('dotenv').config(); // Secret load korche
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
@@ -10,12 +10,15 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(bodyParser.json());
 
-// Database Connection (Secure Way)
+app.use(express.static(__dirname)); 
+
+
+// Database Connection
 mongoose.connect(process.env.MONGO_URL)
     .then(() => console.log("✅ MongoDB Connected!"))
     .catch(err => console.log("❌ DB Error:", err));
 
-// Schema (11 Fields)
+
 const HackathonSchema = new mongoose.Schema({
     name: String,
     organizer: String,
@@ -32,7 +35,6 @@ const HackathonSchema = new mongoose.Schema({
 
 const Hackathon = mongoose.model('Hackathon', HackathonSchema);
 
-// Routes
 app.get('/api/hackathons', async (req, res) => {
     try {
         const hacks = await Hackathon.find().sort({_id: -1});
